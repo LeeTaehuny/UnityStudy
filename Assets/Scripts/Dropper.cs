6dac08965862a33bca08a848c032426e9fa9f5cc
LeeTaes <apth1023@naver.com>
2025-04-26 09:41:13 +0900

    time.time

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        // Time.time : 게임 시작 이후 경과 시간을 보여주는 변수
+        Debug.Log("Time : " + Time.time);
+    }
+}
