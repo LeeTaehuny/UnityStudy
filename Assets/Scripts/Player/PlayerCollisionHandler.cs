ffe3601e50a342aeb3ddbadeefd7982772f93b47
LeeTaes <apth1023@naver.com>
2025-06-11 08:39:51 +0900

    25.06.11

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class PlayerCollisionHandler : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] float collisionCooldown;
+
+    const string hitString = "Hit";
+    float cooldownTimer;
+
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.name);
+        if (cooldownTimer < collisionCooldown) return;
+
+        // 애니메이터에 설정된 hit 트리거를 활성화시킵니다.
+        animator.SetTrigger(hitString);
+
+        // 시간을 초기화합니다.
+        cooldownTimer = 0.0f;
     }
 }
