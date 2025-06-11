38fc2181ced13f08bab2cb0858665fb87cff9863
LeeTaes <apth1023@naver.com>
2025-06-12 08:38:55 +0900

    25.06.12

---
+++ Assets/Scripts/Pickups/Apple.cs
@@ -0,0 +1,10 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Apple : Pickup
+{
+    protected override void OnPickup()
+    {
+        Debug.Log("Power up!");
+    }
+}
--- Assets/Scripts/Pickups/Pickup.cs
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 
-public class Pickup : MonoBehaviour
+public abstract class Pickup : MonoBehaviour
 {
+    [SerializeField] float rotationSpeed;
+
     const string playerString = "Player";
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerString))
         {
-            Debug.Log(other.gameObject.name);   
+            OnPickup();
+            Destroy(gameObject);
         }
     }
+
+    protected void Update()
+    {
+        transform.Rotate(0.0f, rotationSpeed * Time.deltaTime, 0.0f);
+    }
+
+    protected abstract void OnPickup();
 }
